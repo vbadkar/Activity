b40ae204dc1ee31f4229fdb1521a7138369448b2
vbadkar <87715018+vbadkar@users.noreply.github.com>
2021-09-21 00:57:30 +0530

    Changed href links for clean URL

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="http://localhost:8888/htdocs/Activity/" target="_self">
+    <base href="http://localhost:8888/htdocs/Activity/">
     <link href="includes/style.css" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://kit.fontawesome.com/7ed99e45ec.js" crossorigin="anonymous"></script>
@@ -40,11 +40,11 @@
             <ul class='list'>
                 <li><a>Category<i class="fas fa-chevron-down" style="color:black; font-size: 14px; font-weight: 600; padding:5px;"></i></a>
                     <ul>
-                        <li><a href="category.php?category=Food">Food</a></li>
-                        <li><a href="category.php?category=Music">Music</a></li>
-                        <li><a href="category.php?category=Sports">Sports</a></li>
-                        <li><a href="category.php?category=Gymnastics">Gymnastics</a></li>
-                        <li><a href="category.php?category=Travel">Travel</a></li>
+                        <li><a href="category/Food">Food</a></li>
+                        <li><a href="category/Music">Music</a></li>
+                        <li><a href="category/Sports">Sports</a></li>
+                        <li><a href="category/Gymnastics">Gymnastics</a></li>
+                        <li><a href="category/Travel">Travel</a></li>
                     </ul>
                 </li>
                 <li><a href='homepage'>Home</a></li>
