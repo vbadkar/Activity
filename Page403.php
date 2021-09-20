a6ded191dfe1676acde9890b7ffc347d371e04e4
vbadkar <87715018+vbadkar@users.noreply.github.com>
2021-09-20 10:29:44 +0530

    Created Error 403 custom page

---
+++ Page403.php
@@ -0,0 +1,13 @@
+<?php
+  require "includes/header.php";
+?>
+  <div class="error404">
+    <h2>Unauthorized Access</h2>
+    <h1>403</h1>
+    <p>You need to <a href="login">Log-in</a> to view this post</p>
+    <p>Don't have an account<a href="register">Register</a>here</p>
+    <a href="homepage">Return to Homepage</a>
+  </div>
+<?php
+  require "includes/footer1.php";
+?>
