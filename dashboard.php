812cfceec742febd284bf14f270ce87c7fc4a9f5
vbadkar <87715018+vbadkar@users.noreply.github.com>
2021-09-20 10:51:44 +0530

    Fixed nav bar visibility issue on the dashboards

--- dashboard.php
+++ dashboard.php
@@ -17,15 +17,38 @@
     <title>Dashboard</title>
 </head>
 <body>
-    
-    <header>
-        <div class="logo">
-            <h1>Blog<span>$</span></h1>
+<div class="header-wrapper">
+<header>
+            <div class='logo'>
+                <h3>Vozga</h3>
             </div>
-                <ul>
-                    <li><a href="includes/logout.php">Logout</a></li>
-                </ul>
-    </header>
+            <div class="search-form">
+                        <div class="form-toggle">
+                            <form action="search.php" method="get">
+                                <input class="search-text" type="text" name="search" placeholder="Search" autocomplete="off" autofocus><span class="hide"><i class="fas fa-times"></i></span>
+                            </form>
+                        </div>
+                        <div class="icon">
+                            <span class="show"><i class="fas fa-search" style="color:black; font-size: 14px;font-weight: 600;"></i></span>
+                        </div>  
+                    </div>
+                   
+            <img class='hamburger' src='images/hamburger.png' alt='hamburger'></img>
+            <ul class='list'>
+                <li><a>Category<i class="fas fa-chevron-down" style="color:black; font-size: 14px; font-weight: 600; padding:5px;"></i></a>
+                    <ul>
+                        <li><a href="category.php?category=Food">Food</a></li>
+                        <li><a href="category.php?category=Music">Music</a></li>
+                        <li><a href="category.php?category=Sports">Sports</a></li>
+                        <li><a href="category.php?category=Gymnastics">Gymnastics</a></li>
+                        <li><a href="category.php?category=Travel">Travel</a></li>
+                    </ul>
+                </li>
+                <li><a href='homepage'>Home</a></li>
+                <li><a href="logout">Logout</a></li>
+            </ul>
+        </header>
+</div>
     <?php if(isset($_SESSION['message'])):?>
             <div class="msg <?php echo $_SESSION['type'];?>">
                 <li><?php echo $_SESSION['message'];?></li>
@@ -69,26 +92,34 @@
                     <?
                         $input_image="images/".$row['image'];
                         $output_image="images/resized/".$row['image'];
-                        $width=380;
+                        $width=408;
                         $height=220;
                         $resource=imagecreatefromjpeg($input_image);
                         $scaled=imagescale($resource, $width, $height);
                         imagejpeg($scaled,$output_image);
+                        $desc=$row['description'];
+                        $desc = substr($desc,0,100).'...';
                     ?>
-                        <img class="image" src="<?php echo $output_image;?>", alt="post_image">
-                    <div class="post-preview">
-                            <h2><a href="single.php?user_id=<?php echo $userid;?>&id=<?php $row['p_id'];?>" class="post-title"><?php echo $row['title'];?></a></h2>
-                            <p class="preview-text"><?php echo $row['description'];?></p>
-                                <ul class="edit-delete-btn">
-                                    <li>
-                                        <a href="editpost.php?id=<?php echo $row['p_id'];?>" class="btn edit-link">Edit</a>
-                                    </li>
-                                    <li>
-                                        <a href="deletepost.php?del_id=<?php echo  $row['p_id'];?>" class ="btn delete-link" onclick="return confirm('Are you sure you want to delete this?')">Delete</a>
-                                    </li>
-                                </ul>
-                        </div>
-                    </div>
+                    <img class="image" src="<?php echo $output_image;?>", alt="post_image">
+                    <div class="post-preview-wrapper">
+                <a href="single?id=<?php echo $row['p_id'];?>" class="post-title"><?php echo $row['title'];?></a>
+                <div class="post-preview">
+                    <span class="author-name"><i class="fas fa-user"></i>Name</span>
+                    <span class="post-date"><i class="fas fa-calendar-week">Date</i></span>
+                    <p class="desc"><?php echo $desc; ?></p>
+                </div>
+            </div>
+            <div class="btns">
+                <ul class="edit-delete-btn">
+                    <li>
+                        <a href="editpost.php?id=<?php echo $row['p_id'];?>" class="btn edit-link">Edit</a>
+                    </li>
+                    <li>
+                        <a href="deletepost.php?del_id=<?php echo  $row['p_id'];?>" class ="btn delete-link" onclick="return confirm('Are you sure you want to delete this?')">Delete</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
                     <?php
                             $i=$i+1;
                             }
--- user_dashboard.php
+++ user_dashboard.php
@@ -17,95 +17,126 @@
     <title>Dashboard</title>
 </head>
 <body>
-    
-    <header>
-        <div class="logo">
-            <h1>Blog<span>$</span></h1>
+<div class="header-wrapper">
+<header>
+            <div class='logo'>
+                <h3>Vozga</h3>
             </div>
-                <ul>
-                    <li><a href="includes/logout.php">Logout</a></li>
-                </ul>
-    </header>
-    <?php if(isset($_SESSION['message'])):?>
-            <div class="msg <?php echo $_SESSION['type'];?>">
-                <li><?php echo $_SESSION['message'];?></li>
-                <?php 
-                    unset($_SESSION['message']);
-                    unset($_SESSION['type']);
-                ?>  
-            </div>
-        <?php endif;?>
-    <div class="manage-posts-admin">
-            <div class="admin-manage">
-                <ul>
-                    <li><a href="createpost.php">Create Posts</a></li>
-                </ul>
-            </div>
-        <div class="main-content-admin">
-                    <?php
-                        $results_per_page=4;
-                        $pid=$row['p_id'];
-                        $userid=$_COOKIE['cookieuserid'];
-                        $sql="SELECT * FROM posts WHERE user_id='$userid'";
-                        $result=mysqli_query($con,$sql);
-                        //$rowCount = mysqli_num_rows($result);
-                        $i=0;
-                        $num_of_result=mysqli_num_rows($result);    
-                        $num_of_pages=ceil($num_of_result/$results_per_page);
-                        if(!isset($_GET['page'])){
-                            $page=1;
-                        }else{
-                            $page=$_GET['page'];
-                        }
-                        $start_limit=($page-1)*$results_per_page;
-                        $sql="SELECT * FROM posts WHERE user_id='$userid' LIMIT ".$start_limit.','.$results_per_page;
-                        $result=mysqli_query($con,$sql);
-                        if(mysqli_num_rows($result) > $i){
-                            while($row=mysqli_fetch_assoc($result)){
-                    ?> 
-                    <div class="main-post-admin">
-                    <?
-                        $input_image="images/".$row['image'];
-                        $output_image="images/resized/".$row['image'];
-                        $width=380;
-                        $height=220;
-                        $resource=imagecreatefromjpeg($input_image);
-                        $scaled=imagescale($resource, $width, $height);
-                        imagejpeg($scaled,$output_image);
-                    ?>
-                        <img class="image" src="<?php echo $output_image;?>", alt="post_image">
-                    <div class="post-preview">
-                            <h2><a href="single.php?user_id=<?php echo $userid;?>&id=<?php echo $row['p_id'];?>" class="post-title"><?php echo $row['title'];?></a></h2>
-                            <p class="preview-text"><?php echo $row['description'];?></p>
-                                <ul class="edit-delete-btn">
-                                    <li>
-                                        <a href="editpost.php?id=<?php echo $row['p_id'];?>" class="btn edit-link">Edit</a>
-                                    </li>
-                                    <li>
-                                        <a href="deletepost.php?del_id=<?php echo  $row['p_id'];?>" class ="btn delete-link" onclick="return confirm('Are you sure you want to delete this?')">Delete</a>
-                                    </li>
-                                </ul>
+            <div class="search-form">
+                        <div class="form-toggle">
+                            <form action="search.php" method="get">
+                                <input class="search-text" type="text" name="search" placeholder="Search" autocomplete="off" autofocus><span class="hide"><i class="fas fa-times"></i></span>
+                            </form>
                         </div>
+                        <div class="icon">
+                            <span class="show"><i class="fas fa-search" style="color:black; font-size: 14px;font-weight: 600;"></i></span>
+                        </div>  
                     </div>
-                    <?php
-                            $i=$i+1;
-                            }
-                        }
-                        else{
-                            ?>
-                            <tr>
-                                <th colspan="2">No results to display</th>
-                            </tr>
-                            <?php
-                        }
-                    ?>
-        </div>
-        <div class='index-dashboard'><center>
+                   
+            <img class='hamburger' src='images/hamburger.png' alt='hamburger'></img>
+            <ul class='list'>
+                <li><a>Category<i class="fas fa-chevron-down" style="color:black; font-size: 14px; font-weight: 600; padding:5px;"></i></a>
+                    <ul>
+                        <li><a href="category.php?category=Food">Food</a></li>
+                        <li><a href="category.php?category=Music">Music</a></li>
+                        <li><a href="category.php?category=Sports">Sports</a></li>
+                        <li><a href="category.php?category=Gymnastics">Gymnastics</a></li>
+                        <li><a href="category.php?category=Travel">Travel</a></li>
+                    </ul>
+                </li>
+                <li><a href='homepage'>Home</a></li>
+                <li><a href="logout">Logout</a></li>
+            </ul>
+        </header>
+</div>
+<?php if(isset($_SESSION['message'])):?>
+    <div class="msg <?php echo $_SESSION['type'];?>">
+        <li><?php echo $_SESSION['message'];?></li>
+        <?php 
+            unset($_SESSION['message']);
+            unset($_SESSION['type']);
+        ?>  
+    </div>
+<?php endif;?>
+<div class="manage-posts-admin">
+    <div class="admin-manage">
+        <ul>
+            <li><a href="createpost.php">Create Posts</a></li>
+        </ul>
+    </div>
+    <div class="main-content-admin">
         <?php
-        for($page=1;$page<=$num_of_pages;$page++)
-        {
-            echo '<a href="user_dashboard.php?page='.$page.'">'.$page.'</a> ';
-        }   ?> 
+            $results_per_page=4;
+            $pid=$row['p_id'];
+            $userid=$_COOKIE['cookieuserid'];
+            $sql="SELECT * FROM posts WHERE user_id='$userid'";
+            $result=mysqli_query($con,$sql);
+            //$rowCount = mysqli_num_rows($result);
+            $i=0;
+            $num_of_result=mysqli_num_rows($result);    
+            $num_of_pages=ceil($num_of_result/$results_per_page);
+            if(!isset($_GET['page'])){
+                $page=1;
+            }else{
+                $page=$_GET['page'];
+            }
+            $start_limit=($page-1)*$results_per_page;
+            $sql="SELECT * FROM posts WHERE user_id='$userid' LIMIT ".$start_limit.','.$results_per_page;
+            $result=mysqli_query($con,$sql);
+            if(mysqli_num_rows($result) > $i){
+                while($row=mysqli_fetch_assoc($result)){
+        ?> 
+        <div class="main-post-admin">
+            <?php
+                $input_image="images/".$row['image'];
+                $output_image="images/resized/".$row['image'];
+                $width=408;
+                $height=220;
+                $resource=imagecreatefromjpeg($input_image);
+                $scaled=imagescale($resource, $width, $height);
+                imagejpeg($scaled,$output_image);
+                $desc=$row['description'];
+                $desc = substr($desc,0,100).'...';
+            ?>
+            <img class="image" src="<?php echo $output_image;?>", alt="post_image">
+            <div class="post-preview-wrapper">
+                <a href="single?id=<?php echo $row['p_id'];?>" class="post-title"><?php echo $row['title'];?></a>
+                <div class="post-preview">
+                    <span class="author-name"><i class="fas fa-user"></i>Name</span>
+                    <span class="post-date"><i class="fas fa-calendar-week">Date</i></span>
+                    <p class="desc"><?php echo $desc; ?></p>
+                </div>
+            </div>
+            <div class="btns">
+                <ul class="edit-delete-btn">
+                    <li>
+                        <a href="editpost.php?id=<?php echo $row['p_id'];?>" class="btn edit-link">Edit</a>
+                    </li>
+                    <li>
+                        <a href="deletepost.php?del_id=<?php echo  $row['p_id'];?>" class ="btn delete-link" onclick="return confirm('Are you sure you want to delete this?')">Delete</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <?php
+                $i=$i+1;
+                }
+            }
+            else{
+                ?>
+                <tr>
+                    <th colspan="2">No results to display</th>
+                </tr>
+                <?php
+            }
+        ?>
+    </div>
+    <div class='index-dashboard'><center>
+    <?php
+    for($page=1;$page<=$num_of_pages;$page++)
+    {
+        echo '<a href="user_dashboard.php?page='.$page.'">'.$page.'</a> ';
+    }   ?> 
 </div>
 </center>
     </div>
