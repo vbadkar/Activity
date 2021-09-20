e06855fd384cb7818f9282de8285580765785e88
vbadkar <87715018+vbadkar@users.noreply.github.com>
2021-09-20 11:05:15 +0530

    Added base tag with base url of the blogite

--- includes/header.php
+++ includes/header.php
@@ -13,10 +13,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="http://localhost:8888/htdocs/Activity/">
+    <base href="http://localhost:8888/htdocs/Activity/" target="_self">
     <link href="includes/style.css" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://kit.fontawesome.com/7ed99e45ec.js" crossorigin="anonymous"></script>
+    <script src='ckeditor/ckeditor.js'></script>
 
 </head>
 <body style="transform:none">
@@ -35,10 +36,9 @@
                             <span class="show"><i class="fas fa-search" style="color:black; font-size: 14px;font-weight: 600;"></i></span>
                         </div>  
                     </div>
-                   
             <img class='hamburger' src='images/hamburger.png' alt='hamburger'></img>
             <ul class='list'>
-                <li class="sub-list"><a>Category<i class="fas fa-chevron-down" style="color:black; font-size: 14px; font-weight: 600; padding:5px;"></i></a>
+                <li><a>Category<i class="fas fa-chevron-down" style="color:black; font-size: 14px; font-weight: 600; padding:5px;"></i></a>
                     <ul>
                         <li><a href="category.php?category=Food">Food</a></li>
                         <li><a href="category.php?category=Music">Music</a></li>
