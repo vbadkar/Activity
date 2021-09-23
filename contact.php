dfad6f45a2c45d4b22291c487df11f679c3efdd0
Vivek <vivekbadkar@gmail.com>
2021-09-24 01:48:36 +0530

    Adding display messages for subscribing user

--- contact.php
+++ contact.php
@@ -31,6 +31,15 @@
         </div>
     </div>
     <div class="subscribe-us_wrapper">
+    <?php if(isset($_SESSION['message'])):?>
+        <div class="msg <?php echo $_SESSION['type'];?>">
+            <li><?php echo $_SESSION['message'];?></li>
+            <?php 
+                unset($_SESSION['message']);
+                unset($_SESSION['type']);
+            ?>
+        </div>
+    <?php endif;?>
         <h2>
             Become A subscriber
         </h2>
