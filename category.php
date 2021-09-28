9425f755a307dbded4f2e98f896c481f27c6161f
Vivek <vivekbadkar@gmail.com>
2021-09-28 23:19:18 +0530

    Updating theme of category page as per template #53

--- category.php
+++ category.php
@@ -5,30 +5,35 @@
     {
         $category=$_GET['category'];
     }
+    $sql="SELECT login.username FROM login JOIN posts WHERE (login.id = posts.user_id)";
+    $result=mysqli_query($con,$sql);
+    if(mysqli_num_rows($result) > $j){
+        while($authName=mysqli_fetch_assoc($result)){  
+            $author=$authName['username'];
+            $j=$j+1;
+        }
+    }  
 ?>
 <title>Food</title>
 <div class='category-content clear'>
-<div class='main_content'>
-    <h1 class="posts"><?php echo $category;?></h1>
-    <?php
-        $sql="SELECT * FROM posts WHERE category='$category'";
-        $result=mysqli_query($con,$sql);
-        $i=0;
-        if(mysqli_num_rows($result) > $i){
-            while($data=mysqli_fetch_assoc($result)){
-    ?>
-    <div class="main-post">
-        <img class="image" src="images/<?php echo $data['image'];?>", alt="post_image">
-        <div class="post-preview">
-            <h2><a href="single.php"><?php echo $data['title'];?></a></h2>
-            <p class="preview-text"><?php echo $data['description'];?></p>
-        </div>
-    </div>
-    <?php 
+    <div class="wrapper">
+        <div class="posts"><p>Showing posts with label <span>"<?php echo $category;?>"</span></p></div>
+        <div class='main_content'>
+        <?php
+            $sql="SELECT * FROM posts WHERE category='$category'";
+            $result=mysqli_query($con,$sql);
+            $i=0;
+            if(mysqli_num_rows($result) > $i){
+                while($data=mysqli_fetch_assoc($result)){
+        ?>
+        <?php include('includes/card.php'); ?>
+        <?php 
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
 </div>
+<?php include('includes/side_content.php')?>
 <?php
-    require_once "includes/footer1.php";  
+    require_once "includes/footer2.php";
 ?>
