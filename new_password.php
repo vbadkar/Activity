57f2263baf0f6b26f5c9a54d5ff9a8c38bf3521b
Libbna <libbna260296@gmail.com>
2021-09-29 00:37:14 +0530

    reset the commit and made changes to the homepage

---
+++ new_password.php
@@ -0,0 +1,40 @@
+<?php
+    require_once "includes/header.php";
+?>
+<title>Create New Password</title>
+    <div class="register-form">
+    <?php if(isset($_SESSION['message'])):?>
+        <div class="msg <?php echo $_SESSION['type'];?>">
+            <li><?php echo $_SESSION['message'];?></li>
+            <?php 
+                unset($_SESSION['message']);
+                unset($_SESSION['type']);
+            ?>
+        </div>
+    <?php endif;?>
+    <?php
+        $tokenAuth= $_GET['tokenAuth'];
+        $tokenValidator= $_GET['tokenValidator'];
+        if(empty($tokenAuth) || empty($tokenValidator)){
+            echo "Error";
+        }else{
+            if(ctype_xdigit($tokenAuth) !==false && ctype_xdigit($tokenValidator) !==false){
+            ?>
+            <form action='includes/new_pass_validate.php' method='post'>
+                <h1 class="register-title">Create New Password</h1>
+                <input type="hidden" name="tokenAuth" value="<?php echo $tokenAuth;?>">
+                <input type="hidden" name="tokenValidator" value="<?php echo $tokenValidator;?>"> 
+                <input class="text-input" type='password' name='password'placeholder='Enter New Password' id="show-hide">
+                <input class="text-input" type='password' name='confirmPass'placeholder='Confirm New Password' id="show-hide"><br>
+                <button class="register-button" type='submit' name='submit-new-pass'>Reset Password</button>
+            </form>
+            <?php
+            }
+        }
+
+    ?>
+        
+    </div>
+<?php
+    require_once "includes/footer1.php";
+?>
