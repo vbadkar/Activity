e2a9d6730d3649cfc4084273d34687f080a43713
vbadkar <vivekbadkar@gmail.com>
2021-10-24 16:42:46 +0530

    Added option in admin panel to add languages

---
+++ add-language.php
@@ -0,0 +1,24 @@
+<?php
+    require_once "includes/header.php";
+?>
+<title>Add language</title>
+    <div class="register-form">
+        <?php if(isset($_SESSION['message'])):?>
+            <div class="msg <?php echo $_SESSION['type'];?>">
+                <li><?php echo $_SESSION['message'];?></li>
+                <?php 
+                    unset($_SESSION['message']);
+                    unset($_SESSION['type']);
+                ?>
+            </div>
+        <?php endif;?>
+        <form action='includes/add-language-inc.php' method='post'>
+            <h1 class="register-title">Add language</h1>
+            <input class="text-input" type='text' name='language' placeholder='Enter a language' autocomplete='off'>
+            <input class="text-input" type='text' name='lang_code' placeholder='Enter a language code(e.g. en for English)' autocomplete='off'>
+            <button class="register-button" type='submit' name='submit'>Add</button>
+        </form>
+    </div>
+<?php
+    require_once "includes/footer1.php";
+?>
---
+++ includes/add-language-inc.php
@@ -0,0 +1,60 @@
+<?php
+    if(isset($_POST['submit']))
+    {
+        require "database.php";
+        session_start();
+        $language=$_POST['language'];
+        $lang_code=$_POST['lang_code'];
+        if(empty($language) || empty($lang_code))
+        {
+            $_SESSION['message']="Fields are blank";
+            $_SESSION['type']="error";
+            header("Location: ../add-language.php?error=emptyfields");
+            exit();
+        }
+        else
+        {
+            $sql="SELECT lang_name FROM languages WHERE lang_name = ?";
+            $stmt=mysqli_stmt_init($con);   
+            if(!mysqli_stmt_prepare($stmt, $sql))
+            {
+                header("Location: ../dashboard.php?error=sqlerror");
+                exit();
+            }
+            else
+            {
+                mysqli_stmt_bind_param($stmt,"s",$language);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_store_result($stmt);
+                $count = mysqli_stmt_num_rows($stmt);
+                if($count>0)
+                {
+                    $_SESSION['message']="Language already exists";
+                    $_SESSION['type']="error";
+                    header("Location: ../dashboard.php?error=languagealreadyexists");
+                    exit();
+                }
+                else
+                {
+                    $sql="INSERT INTO languages (lang_name, lang_code) VALUES (?, ?)";
+                    $stmt=mysqli_stmt_init($con);
+                    if(!mysqli_stmt_prepare($stmt, $sql))
+                    {
+                        header("Location: ../dashboard.php?error=sqlerror");
+                        exit();
+                    }
+                    else
+                    {
+                        mysqli_stmt_bind_param($stmt,"ss",$language, $lang_code);
+                        mysqli_stmt_execute($stmt);
+                        $_SESSION['message']="Language added sucessfully";
+                        $_SESSION['type']="success";
+                        header("Location: ../add-language.php?success=languageadded");
+                        $msg= "Language added sucessfullly";
+                        exit();
+                    }
+                }
+            }
+        }
+    }
+?>
