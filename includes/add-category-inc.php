b1406a14704329df4f03792e4b5d79ea5d0d3089
vbadkar <vivekbadkar@gmail.com>
2021-10-27 07:44:55 +0530

    Added lang_code column in database for translation of categories

--- includes/add-category-inc.php
+++ includes/add-category-inc.php
@@ -4,8 +4,10 @@
         require "database.php";
         session_start();
         $category=$_POST['category'];
+        $lang_code=$_POST['lang_code'];
+        $dup_id = 0;
         
-        if(empty($category))
+        if(empty($category) || empty($lang_code))
         {
             $_SESSION['message']="No category entered";
             $_SESSION['type']="error";
@@ -36,7 +38,7 @@
                 }
                 else
                 {
-                    $sql="INSERT INTO categories (cat_name) VALUES (?)";
+                    $sql="INSERT INTO categories (dup_id, cat_name, lang_code) VALUES (?, ?, ?)";
                     $stmt=mysqli_stmt_init($con);
                     if(!mysqli_stmt_prepare($stmt, $sql))
                     {
@@ -45,7 +47,7 @@
                     }
                     else
                     {
-                        mysqli_stmt_bind_param($stmt,"s",$category);
+                        mysqli_stmt_bind_param($stmt,"iss", $dup_id, $category, $lang_code);
                         mysqli_stmt_execute($stmt);
                         $_SESSION['message']="Category added sucessfully";
                         $_SESSION['type']="success";
