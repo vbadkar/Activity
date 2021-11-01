35237a542e2b84c55fd49b0b9208e7add242e8c8
vbadkar <vivekbadkar@gmail.com>
2021-11-01 08:29:21 +0530

    Changed href as per clean URL

--- includes/footer2.php
+++ includes/footer2.php
@@ -1,9 +1,9 @@
 </body>
 <footer class="static">
     <ul>
-        <li><a href="homepage.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="contact.php">Contact</a></li>
+        <li><a href="homepage">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="contact">Contact</a></li>
     </ul>
     <p>Designed with <span><i class="fas fa-heart"></i></span> by <span class="brand-name">vbad</span></p>
 </footer>
